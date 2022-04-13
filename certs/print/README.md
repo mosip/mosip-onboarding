e3de2ebc37d4d40a4dfe119b904c5b1483f97cf2
Puneet Joshi <puneet@mosip.io>
2022-04-13 14:48:54 +0530

    [MOSIP-21369] WIP.

---
+++ certs/print/README.md
@@ -0,0 +1,3 @@
+The \*-inline.pem file contains the same cert in a single line with "\n" character inserted for line end. If you change the main cert, make sure this file is also changed. 
+ 
+
--- default.sh
+++ default.sh
@@ -7,6 +7,7 @@
 
 mydir=`pwd`
 DATE=`date -u +%FT%T.%3NZ`
+URL=https://api-internal.soil.mosip.net
 ENV_URL=
 KEYCLOAK_URL=
 KEYCLOAK_ADMIN_USERNAME=admin
@@ -15,121 +16,115 @@ CERT_MANAGER=mosip-deployment-client
 #KEYCLOAK_ADMIN_PASSWORD=
 #CERT_MANAGER_PASSWORD=
 
-echo "Uploading ida root cert" 
-newman run onboarding.postman_collection.json --delay-request 2000 -e onboarding.postman_environment.json \
---env-var cert-application-id=ROOT \
---env-var cert-reference-id=  \
---env-var request-time=$DATE \
---env-var cert-manager-username=$CERT_MANAGER \
---env-var cert-manager-password=$CERT_MANAGER_PASSWORD \
---env-var partner-domain=AUTH \
---folder authenticate-to-download-certs \
---folder download-ida-certificate \
---folder upload-ca-certificate \
--r htmlextra --reporter-htmlextra-export ./reports/ida-root.html
-
-echo "Uploading ida cert"
-newman run onboarding.postman_collection.json --delay-request 2000 -e onboarding.postman_environment.json \
---env-var cert-application-id=IDA \
---env-var cert-reference-id=  \
---env-var request-time=$DATE \
---env-var cert-manager-username=$CERT_MANAGER \
---env-var cert-manager-password=$CERT_MANAGER_PASSWORD \
---env-var partner-domain=AUTH \
---folder authenticate-to-download-certs \
---folder download-ida-certificate \
---folder upload-ca-certificate \
--r htmlextra --reporter-htmlextra-export ./reports/ida-ca.html
-
-echo "Uploading mpartner-default-auth cert"
-newman run onboarding.postman_collection.json --delay-request 2000 -e onboarding.postman_environment.json \
---env-var request-time=$DATE \
---env-var cert-application-id=IDA \
---env-var cert-reference-id=mpartner-default-auth \
---env-var cert-manager-username=$CERT_MANAGER \
---env-var cert-manager-password=$CERT_MANAGER_PASSWORD \
---env-var keycloak-admin-username=$KEYCLOAK_ADMIN_USER \
---env-var keycloak-admin-password=$KEYCLOAK_ADMIN_PASSWORD \
---env-var partner-kc-username=mpartner-default-auth \
---env-var partner-domain=AUTH \
---folder authenticate-to-download-certs \
---folder download-ida-certificate \
---folder upload-leaf-certificate \
---folder upload-other-domain-certificate-to-keymanager \
--r htmlextra --reporter-htmlextra-export ./reports/ida-leaf.html --reporter-htmlextra-showEnvironmentData 
-
-exit
-echo "Uploading  ida cred cert to keymanager"
-newman run onboarding.postman_collection.json --delay-request 2000 -e onboarding.postman_environment.json \
---env-var request-time=$DATE \
---env-var cert-application-id=IDA \
---env-var cert-reference-id=CRED_SERVICE \
---env-var cert-manager-username=$CERT_MANAGER \
---env-var cert-manager-password=$CERT_MANAGER_PASSWORD \
---env-var keycloak-admin-username=$KEYCLOAK_ADMIN_USER \
---env-var keycloak-admin-password=$KEYCLOAK_ADMIN_PASSWORD \
---env-var partner-kc-username=mpartner-default-auth \
---env-var partner-domain=AUTH \
---folder authenticate-to-download-certs \
---folder download-ida-certificate \
---folder upload-ida-cred-cert-to-keymanager \
--r htmlextra --reporter-htmlextra-export ./reports/ida-cred.html --reporter-htmlextra-showEnvironmentData 
-
-echo "Uploading mpartner-default-resident cert"
-newman run onboarding.postman_collection.json --delay-request 2000 -e onboarding.postman_environment.json \
---env-var request-time=$DATE \
---env-var cert-application-id=RESIDENT \
---env-var cert-reference-id=mpartner-default-resident \
---env-var cert-manager-username=$CERT_MANAGER \
---env-var cert-manager-password=$CERT_MANAGER_PASSWORD \
---env-var keycloak-admin-username=$KEYCLOAK_ADMIN_USER \
---env-var keycloak-admin-password=$KEYCLOAK_ADMIN_PASSWORD \
---env-var partner-kc-username=mpartner-default-resident \
---env-var partner-domain=AUTH \
---folder authenticate-to-download-certs \
---folder download-ca-certificate-from-keymanager \
---folder download-leaf-certificate-from-keymanager \
---folder upload-ca-certificate \
---folder upload-leaf-certificate \
---folder upload-signed-leaf-certifcate-to-keymanager \
--r htmlextra --reporter-htmlextra-export ./reports/resident.html --reporter-htmlextra-showEnvironmentData 
-
-echo "Uploading mpartner-default-print cert"
-root_cert_path="$mydir/certs/print/RootCA.pem"
-client_cert_path="$mydir/certs/print/Client.pem"
-RootCACert=`awk 'NF {sub(/\r/, ""); printf "%s\\r\\n",$0;}' $root_cert_path`
-PartnerCert=`awk 'NF {sub(/\r/, ""); printf "%s\\r\\n",$0;}' $client_cert_path`
-newman run onboarding.postman_collection.json --delay-request 2000 -e onboarding.postman_environment.json \
---env-var request-time=$DATE \
---env-var partner-manager-username=$CERT_MANAGER \
---env-var partner-manager-password=$CERT_MANAGER_PASSWORD \
---env-var keycloak-admin-username=$KEYCLOAK_ADMIN_USER \
---env-var keycloak-admin-password=$KEYCLOAK_ADMIN_PASSWORD \
---env-var partner-kc-username=mpartner-default-print \
---env-var application-id=ida \
---env-var partner-domain=AUTH \
---env-var ca-certificate="$RootCACert" \
---env-var leaf-certificate="$PartnerCert" \
---folder authenticate-to-upload-certs \
---folder upload-ca-certificate \
---folder upload-leaf-certificate \
---folder upload-signed-leaf-certifcate-to-keymanager \
--r htmlextra --reporter-htmlextra-export ./reports/print.html --reporter-htmlextra-showEnvironmentData 
-
-#echo "uploading mpartner default print cert" $'\n'
-#root_cert_path="$mydir/certs/print/RootCA.pem"
-#client_cert_path="$mydir/certs/print/Client.pem"
-#RootCACert=$(awk 'NF {sub(/\r/, ""); printf "%s\\r\\n",$0;}' $root_cert_path)
-#PartnerCert=$(awk 'NF {sub(/\r/, ""); printf "%s\\r\\n",$0;}' $client_cert_path)
-#jq '.values |= map(if .key=="partner-kc-username" then (.value="mpartner-default-print") else . end)' onboarding.postman_environment.json > $(prop 'tmp_dir')/tmp.json && mv $(prop 'tmp_dir')/tmp.json onboarding.postman_environment.json
-#newman run onboarding.postman_collection.json --delay-request 2000 -e onboarding.postman_environment.json --export-environment $env_temp_file/onboarding.postman_environment.json \
-#--folder authenticate-to-upload-certs \
-#--env-var ca-certificate="$RootCACert" \
-#--env-var leaf-certificate="$PartnerCert" \
+#echo "Uploading ida root cert" 
+#newman run onboarding.postman_collection.json --delay-request 2000 -e onboarding.postman_environment.json \
+#--env-var url=$URL \
+#--env-var cert-application-id=ROOT \
+#--env-var cert-reference-id=  \
+#--env-var request-time=$DATE \
+#--env-var cert-manager-username=$CERT_MANAGER \
+#--env-var cert-manager-password=$CERT_MANAGER_PASSWORD \
+#--env-var partner-domain=AUTH \
+#--folder authenticate-as-cert-manager \
+#--folder download-ida-certificate \
 #--folder upload-ca-certificate \
+#-r htmlextra --reporter-htmlextra-export ./reports/ida-root.html
+#
+#echo "Uploading ida cert"
+#newman run onboarding.postman_collection.json --delay-request 2000 -e onboarding.postman_environment.json \
+#--env-var url=$URL \
+#--env-var cert-application-id=IDA \
+#--env-var cert-reference-id=  \
+#--env-var request-time=$DATE \
+#--env-var cert-manager-username=$CERT_MANAGER \
+#--env-var cert-manager-password=$CERT_MANAGER_PASSWORD \
+#--env-var partner-domain=AUTH \
+#--folder authenticate-as-cert-manager \
+#--folder download-ida-certificate \
+#--folder upload-ca-certificate \
+#-r htmlextra --reporter-htmlextra-export ./reports/ida-ca.html
+#
+#echo "Uploading mpartner-default-auth cert"
+#newman run onboarding.postman_collection.json --delay-request 2000 -e onboarding.postman_environment.json \
+#--env-var url=$URL \
+#--env-var request-time=$DATE \
+#--env-var cert-application-id=IDA \
+#--env-var cert-reference-id=mpartner-default-auth \
+#--env-var cert-manager-username=$CERT_MANAGER \
+#--env-var cert-manager-password=$CERT_MANAGER_PASSWORD \
+#--env-var keycloak-admin-username=$KEYCLOAK_ADMIN_USER \
+#--env-var keycloak-admin-password=$KEYCLOAK_ADMIN_PASSWORD \
+#--env-var partner-kc-username=mpartner-default-auth \
+#--env-var partner-domain=AUTH \
+#--folder download-ida-certificate \
 #--folder upload-leaf-certificate \
-#-r htmlextra --reporter-htmlextra-export $(prop 'report_dir')/mapartner-default-print.html
-#rm $env_temp_file/*
+#--folder upload-other-domain-certificate-to-keymanager \
+#-r htmlextra --reporter-htmlextra-export ./reports/ida-leaf.html --reporter-htmlextra-showEnvironmentData 
+#
+#echo "Uploading ida cred cert to keymanager"
+#newman run onboarding.postman_collection.json --delay-request 2000 -e onboarding.postman_environment.json \
+#--env-var url=$URL \
+#--env-var request-time=$DATE \
+#--env-var cert-application-id=IDA \
+#--env-var cert-reference-id=CRED_SERVICE \
+#--env-var cert-manager-username=$CERT_MANAGER \
+#--env-var cert-manager-password=$CERT_MANAGER_PASSWORD \
+#--env-var keycloak-admin-username=$KEYCLOAK_ADMIN_USER \
+#--env-var keycloak-admin-password=$KEYCLOAK_ADMIN_PASSWORD \
+#--env-var partner-kc-username=mpartner-default-auth \
+#--env-var partner-domain=AUTH \
+#--folder authenticate-as-cert-manager \
+#--folder download-ida-certificate \
+#--folder upload-ida-cred-cert-to-keymanager \
+#-r htmlextra --reporter-htmlextra-export ./reports/ida-cred.html --reporter-htmlextra-showEnvironmentData 
+#
+
+upload_resident() {
+    echo "Uploading mpartner-default-resident cert"
+    newman run onboarding.postman_collection.json --delay-request 2000 -e onboarding.postman_environment.json \
+    --env-var url=$URL \
+    --env-var request-time=$DATE \
+    --env-var cert-application-id=RESIDENT \
+    --env-var cert-reference-id=mpartner-default-resident \
+    --env-var cert-manager-username=$CERT_MANAGER \
+    --env-var cert-manager-password=$CERT_MANAGER_PASSWORD \
+    --env-var keycloak-admin-username=$KEYCLOAK_ADMIN_USER \
+    --env-var keycloak-admin-password=$KEYCLOAK_ADMIN_PASSWORD \
+    --env-var partner-kc-username=mpartner-default-resident \
+    --env-var partner-domain=AUTH \
+    --folder authenticate-as-cert-manager \
+    --folder download-ca-certificate-from-keymanager \
+    --folder download-leaf-certificate-from-keymanager \
+    --folder upload-ca-certificate \
+    --folder upload-leaf-certificate \
+    --folder upload-signed-leaf-certifcate-to-keymanager \
+    -r htmlextra --reporter-htmlextra-export ./reports/resident.html --reporter-htmlextra-showEnvironmentData 
+}
+upload_print() {
+    echo "Uploading mpartner-default-print cert"
+    root_cert_path="$mydir/certs/print/root-ca-inline.pem"
+    partner_cert_path="$mydir/certs/print/client-inline.pem"
+    root_ca_cert=`awk '{ print $0 }' $root_cert_path`
+    partner_cert=`awk '{ print $0 }' $partner_cert_path`
+    newman run onboarding.postman_collection.json --delay-request 2000 -e onboarding.postman_environment.json \
+    --env-var url=$URL \
+    --env-var request-time=$DATE \
+    --env-var cert-manager-username=$CERT_MANAGER \
+    --env-var cert-manager-password=$CERT_MANAGER_PASSWORD \
+    --env-var partner-kc-username=mpartner-default-print \
+    --env-var application-id=ida \
+    --env-var partner-domain=AUTH \
+    --env-var ca-certificate="$root_ca_cert" \
+    --env-var leaf-certificate="$partner_cert" \
+    --folder authenticate-as-cert-manager \
+    --folder upload-ca-certificate \
+    --folder upload-leaf-certificate \
+    -r htmlextra --reporter-htmlextra-export ./reports/print.html --reporter-htmlextra-showEnvironmentData 
+}
+
+upload_resident
+upload_print
+
 #
 #echo "uploading mpartner default abis cert" $'\n'
 #root_cert_path="$mydir/certs/abis/RootCA.pem"
